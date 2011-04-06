8a0ae47c45dfa7f52e864ed76562161429812eaf
nelsonglory <nelsonglory@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-06 10:24:30 +0000

    new release 0.3.9 uploaded
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ya_googlesearch/tags/0.3.9@46267 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "ya_googlesearch".
+#
+# Auto generated 11-11-2010 13:48
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Yet another Google search',
+	'description' => 'Flexible Google search & display plugin',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.3.9',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Roderick Braun',
+	'author_email' => 'roderick.braun@ph-freiburg.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"ff24";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"6c94";s:17:"ext_localconf.php";s:4:"25a0";s:14:"ext_tables.php";s:4:"b27b";s:13:"locallang.xml";s:4:"9a9b";s:16:"locallang_db.xml";s:4:"1cf9";s:15:"css/default.css";s:4:"478d";s:14:"doc/manual.sxw";s:4:"30e1";s:19:"doc/wizard_form.dat";s:4:"fa3a";s:20:"doc/wizard_form.html";s:4:"1c5a";s:19:"images/backward.png";s:4:"a7ee";s:18:"images/forward.png";s:4:"0736";s:17:"images/google.png";s:4:"47ad";s:14:"pi1/ce_wiz.gif";s:4:"dc0f";s:35:"pi1/class.tx_yagooglesearch_pi1.php";s:4:"1c02";s:43:"pi1/class.tx_yagooglesearch_pi1_wizicon.php";s:4:"f161";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"e557";s:24:"pi1/static/editorcfg.txt";s:4:"17e6";s:35:"pi2/class.tx_yagooglesearch_pi2.php";s:4:"71d0";s:17:"pi2/locallang.xml";s:4:"0987";s:24:"pi2/static/editorcfg.txt";s:4:"9d94";s:23:"templates/template.html";s:4:"1eba";s:34:"templates/template_searchonly.html";s:4:"ef7a";}',
+);
+
+?>
