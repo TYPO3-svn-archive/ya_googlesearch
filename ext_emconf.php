0f826c00002866f142491c4fefff96ce5b8392bc
nelsonglory <nelsonglory@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-25 10:32:08 +0000

    new release 0.3.12
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ya_googlesearch/trunk@78324 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "ya_googlesearch".
-#
-# Auto generated 11-11-2010 13:48
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "ya_googlesearch".
+ *
+ * Auto generated 25-07-2013 12:26
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Yet another Google search',
 	'description' => 'Flexible Google search & display plugin',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.3.11',
+	'version' => '0.3.12',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -41,7 +41,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"ff24";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"6c94";s:17:"ext_localconf.php";s:4:"25a0";s:14:"ext_tables.php";s:4:"b27b";s:13:"locallang.xml";s:4:"9a9b";s:16:"locallang_db.xml";s:4:"1cf9";s:15:"css/default.css";s:4:"478d";s:14:"doc/manual.sxw";s:4:"30e1";s:19:"doc/wizard_form.dat";s:4:"fa3a";s:20:"doc/wizard_form.html";s:4:"1c5a";s:19:"images/backward.png";s:4:"a7ee";s:18:"images/forward.png";s:4:"0736";s:17:"images/google.png";s:4:"47ad";s:14:"pi1/ce_wiz.gif";s:4:"dc0f";s:35:"pi1/class.tx_yagooglesearch_pi1.php";s:4:"1c02";s:43:"pi1/class.tx_yagooglesearch_pi1_wizicon.php";s:4:"f161";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"e557";s:24:"pi1/static/editorcfg.txt";s:4:"17e6";s:35:"pi2/class.tx_yagooglesearch_pi2.php";s:4:"71d0";s:17:"pi2/locallang.xml";s:4:"0987";s:24:"pi2/static/editorcfg.txt";s:4:"9d94";s:23:"templates/template.html";s:4:"1eba";s:34:"templates/template_searchonly.html";s:4:"ef7a";}',
+	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"ff24";s:12:"ext_icon.gif";s:4:"6c94";s:17:"ext_localconf.php";s:4:"25a0";s:14:"ext_tables.php";s:4:"b27b";s:13:"locallang.xml";s:4:"9a9b";s:16:"locallang_db.xml";s:4:"1cf9";s:10:"README.txt";s:4:"ee2d";s:15:"css/default.css";s:4:"df85";s:14:"doc/manual.sxw";s:4:"c576";s:19:"doc/wizard_form.dat";s:4:"fa3a";s:20:"doc/wizard_form.html";s:4:"1c5a";s:19:"images/backward.png";s:4:"a7ee";s:18:"images/forward.png";s:4:"0736";s:17:"images/google.png";s:4:"12ba";s:14:"pi1/ce_wiz.gif";s:4:"dc0f";s:35:"pi1/class.tx_yagooglesearch_pi1.php";s:4:"5e7e";s:43:"pi1/class.tx_yagooglesearch_pi1_wizicon.php";s:4:"fffc";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"e557";s:35:"pi2/class.tx_yagooglesearch_pi2.php";s:4:"fa48";s:17:"pi2/locallang.xml";s:4:"7496";s:23:"templates/template.html";s:4:"adb1";}',
+	'suggests' => array(
+	),
 );
 
 ?>
